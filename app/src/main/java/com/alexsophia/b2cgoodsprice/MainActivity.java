4d22e926531d00c60b32db0ae05b1c44302d1243
Alexander0024 <lizhe0024@126.com>
2016-10-24 14:54:40 +0800

    Initial commit

---
+++ app/src/main/java/com/alexsophia/b2cgoodsprice/MainActivity.java
@@ -0,0 +1,13 @@
+package com.alexsophia.b2cgoodsprice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
