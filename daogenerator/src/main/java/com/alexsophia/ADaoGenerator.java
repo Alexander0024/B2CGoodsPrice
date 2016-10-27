247da12f13db0f23854b638d91fc1134aa2bb926
Alexanderlwz <Alexander0024>
2016-10-27 20:17:46 +0800

    Update gao generator(not finished yet). Just sync to mac.

--- daogenerator/src/main/java/com/alexsophia/ADaoGenerator.java
+++ daogenerator/src/main/java/com/alexsophia/ADaoGenerator.java
@@ -2,38 +2,77 @@ package com.alexsophia;
 
 import de.greenrobot.daogenerator.DaoGenerator;
 import de.greenrobot.daogenerator.Entity;
-import de.greenrobot.daogenerator.Property;
 import de.greenrobot.daogenerator.Schema;
 
 public class ADaoGenerator {
     public static void main(String[] args) throws Exception {
         Schema schema = new Schema(1, "greendao");
         addGoodsEntity(schema);
+        addGoodsPriceEntity(schema);
+        addPriceTypeEntity(schema);
+        addGoodsUrlEntity(schema);
         new DaoGenerator().generateAll(schema, "./../B2CGoodsPrice/app/src/main/java-gen");
     }
 
+    /**
+     * Add Goods table.
+     */
     private static void addGoodsEntity(Schema schema) {
         Entity goods = schema.addEntity("Goods");
         goods.addIdProperty().primaryKey().autoincrement();
-        Property goodsId = goods.addLongProperty("goodsId").getProperty();
+        // Goods type 类别
         goods.addStringProperty("type");
+        // Goods brand 厂家
         goods.addStringProperty("brand");
+        // Goods name 名称
         goods.addStringProperty("name");
+        // Goods standard 规格
         goods.addStringProperty("standard");
-        goods.addDoubleProperty("cheapest_online");
-        goods.addDoubleProperty("cheapest_offline");
+        // Goods cheapest online
+        goods.addDoubleProperty("cheapestOnline");
+        // Goods cheapest offline
+        goods.addDoubleProperty("cheapestOffline");
+    }
 
-        Entity urls = schema.addEntity("GoodsUrls");
-        urls.addIdProperty().primaryKey();
-        urls.addStringProperty("url");
-        goods.addToMany(urls, goodsId).setName("urls");
-
+    /**
+     * Add price table
+     */
+    private static void addGoodsPriceEntity(Schema schema) {
         Entity prices = schema.addEntity("GoodsPrices");
-        prices.addIdProperty().primaryKey();
-        prices.addBooleanProperty("type");
+        prices.addIdProperty().primaryKey().autoincrement();
+        // foreign key - goods id
+        prices.addLongProperty("goodsId");
+        // foreign key - type id
+        prices.addLongProperty("typeId");
+        // price date 日期
         prices.addDateProperty("date");
+        // price seller 卖家
         prices.addStringProperty("seller");
+        // price 价格
         prices.addDoubleProperty("price");
-        goods.addToMany(prices, goodsId).setName("prices");
+        // price url 链接
+        prices.addStringProperty("url");
+    }
+
+    /**
+     * Add price type table
+     */
+    private static void addPriceTypeEntity(Schema schema){
+        Entity priceType = schema.addEntity("PriceType");
+        priceType.addIdProperty().primaryKey().autoincrement();
+        // Type Name 分类的名称
+        priceType.addStringProperty("typeName");
+    }
+
+    /**
+     * Add goods url table
+     */
+    private static void addGoodsUrlEntity(Schema schema) {
+        Entity urls = schema.addEntity("GoodsUrls");
+        urls.addIdProperty().primaryKey().autoincrement();
+        // foreign key - goods id
+        urls.addLongProperty("goodsId");
+        // url address
+        urls.addStringProperty("url");
     }
 }
