6fca9289bc2b6806c013b9482295de624134de77
Alexander0024 <lizhe0024@126.com>
2016-10-28 16:44:27 +0800

    Code Optimization.

--- app/src/main/java/com/alexsophia/b2cgoodsprice/features/manage/ui/ManageMainFragment.java
+++ app/src/main/java/com/alexsophia/b2cgoodsprice/features/manage/ui/ManageMainFragment.java
@@ -15,8 +15,10 @@ import butterknife.OnClick;
  * Created by Alexander on 2016/10/28.
  */
 public class ManageMainFragment extends BaseFragment {
-    @Bind(R.id.rl_manage_goods)
-    AutoRelativeLayout mRlManageGoods;
+    @Bind(R.id.rl_manage_goods_type)
+    AutoRelativeLayout mRlManageGoodsType;
+    @Bind(R.id.rl_manage_goods_brand)
+    AutoRelativeLayout mRlManageGoodsBrand;
 
     public static ManageMainFragment newInstance() {
         return new ManageMainFragment();
@@ -52,11 +54,13 @@ public class ManageMainFragment extends BaseFragment {
 
     }
 
-    @OnClick(R.id.rl_manage_goods)
+    @OnClick({R.id.rl_manage_goods_type, R.id.rl_manage_goods_brand})
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.rl_manage_goods:
-                startActivity(getActivity(), GoodsManageActivity.class);
+            case R.id.rl_manage_goods_type:
+                startActivity(getActivity(), GoodsTypeManageActivity.class);
+                break;
+            case R.id.rl_manage_goods_brand:
                 break;
         }
     }
